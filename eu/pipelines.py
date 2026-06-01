12ba258d0d46f7b3624dde3fc2621e57fae898a6
tnrtnch <tnrtnch@abv.bg>
2026-06-01 17:29:44 +0300

    fix pipelines

--- eu/pipelines.py
+++ eu/pipelines.py
@@ -4,35 +4,30 @@ from datetime import datetime, UTC
 
 
 class EuPipeline:
+
     def open_spider(self, spider):
         spider.logger.info("PIPELINE OPENED")
 
         self.items = []
 
-        output_file = spider.config.get("output_file")
+        self.json_path = Path(
+            "eu_fsf_sanctions.json"
+        ).resolve()
 
-        spider.logger.info(f"OUTPUT FILE: {output_file}")
-
-        if not output_file:
-            raise Exception("output_file missing in scraper.yaml")
-
-        self.json_path = Path(output_file).resolve()
-
-        spider.logger.info(f"JSON PATH: {self.json_path}")
+        spider.logger.info(
+            f"JSON PATH: {self.json_path}"
+        )
 
     def process_item(self, item, spider):
-        spider.logger.info(f"PROCESS ITEM: {item}")
-
         self.items.append(dict(item))
-
         return item
 
     def close_spider(self, spider):
         spider.logger.info("CLOSING SPIDER")
+
         if len(self.items) < 100:
             raise Exception(
-                f"Refusing to write suspicious dataset "
-                f"with only {len(self.items)} items"
+                f"Too few items: {len(self.items)}"
             )
 
         output = {
@@ -49,5 +44,5 @@ class EuPipeline:
             )
 
         spider.logger.info(
-            f"{len(self.items)} records written to {self.json_path}"
+            f"{len(self.items)} records written"
         )
