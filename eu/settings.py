8176d66ae40892358c59cee1c5e65044d4faf54a
tnrtnch <tnrtnch@abv.bg>
2026-06-01 16:16:56 +0300

    fix settings

--- eu/settings.py
+++ eu/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = "eu.spiders"
 #USER_AGENT = "eu (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = False
+ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
